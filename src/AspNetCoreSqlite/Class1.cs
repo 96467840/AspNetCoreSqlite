2c1eef088cce720de1e7326c1d233d148a1b7cdb
Andrey Murin <96467840@mail.ru>
2017-03-25 17:22:30 +0300

    обновление проекта до VS 2017

--- src/AspNetCoreSqlite/Class1.cs
+++ src/AspNetCoreSqlite/Class1.cs
@@ -1,14 +1,20 @@
-﻿using System;
+﻿using AspNetCoreComponentLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AspNetCoreSqlite
 {
-    public class Class1
+    public class Class1 : BaseDM
     {
         public Class1()
         {
+        }
+
+        public override void FromDB()
+        {
+            throw new NotImplementedException();
         }
     }
 }
